28597de3e44d84c5ad1ab3162b819976fa43020a
Haocheng Hu <hc.hu@utoronto.ca>
2021-02-20 23:34:29 -0500

    fix dependencies

--- achievement-tracker/src/App.js
+++ achievement-tracker/src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 import Home from './react-components/Home'
