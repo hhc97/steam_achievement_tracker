661dff33de3009addbaf0405e26dff0302b3af40
Zexi Wang <walker.wang@mail.utoronto.ca>
2021-03-10 16:59:08 -0800

    edit functionality done

--- achievement-tracker/src/react-components/AccountSettings/index.js
+++ achievement-tracker/src/react-components/AccountSettings/index.js
@@ -3,10 +3,29 @@ import { HeaderButton, HeadContainer, HeaderNavBar, HeaderImage } from '../Heade
 
 import sampleProfilePic from "./imgs/sampleProfilePic.jpg"
 import logo from './../../logo.svg'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./style.css"
 
 class AccountSettings extends React.Component {
+    state = {
+        steamInput: "PolarisTM",
+        ubisoftInput: "Polaris04",
+        playstationInput: "Dancin9D0nZ",
+        xboxInput: "N/A"
+    }
+
+    editAction = input => {
+        if (document.getElementById(input).className === "hide") {
+            document.getElementById(input).className="show";
+        } else {
+            document.getElementById(input).className="hide"
+            this.setState({
+                [input]: document.getElementById(input).value
+            })
+        }
+    }
+
     render() {
         return (
             <div id="AccountSettingsPage">
@@ -39,25 +58,45 @@ class AccountSettings extends React.Component {
                 <div id="UserDetailsSection">
                     <h2> User Details </h2>
                     <div className="TextInput+Button">
-                        <p> Username: <span> user1 </span>  <button className="editButton">  </button> </p>
+                        <p>Username: <span> user1 </span></p> 
                     </div>
                     <div className="TextInput+Button">
-                        <p> Password: <span> **** </span> <button className="editButton">  </button> </p>
+                        <p> Password: <span> **** </span></p>
                     </div>
                 </div>
                 <div id="ExternalLinksSection">
                     <h2> External Links </h2>
-                    <div className="TextInput+Button">
-                        <p> Steam: <span> PolarisTM </span> <button className="editButton">  </button> </p>
+
+                    <div className="TextInput-Button">
+                        <p>Steam: <span>{this.state.steamInput}</span></p>
+                        <input className="hide" type="text" id="steamInput"></input>
+                        <button type="button" className="btn btn-secondary" onClick={() => {this.editAction("steamInput")}}>
+                            Edit
+                        </button>
                     </div>
+
                     <div className="TextInput+Button">
-                        <p> Ubisoft: <span> Polaris04 </span> <button className="editButton">  </button> </p>
+                        <p>Ubisoft: <span>{this.state.ubisoftInput}</span></p>
+                        <input className="hide" type="text" id="ubisoftInput"></input>
+                        <button type="button" className="btn btn-secondary" onClick={() => {this.editAction("ubisoftInput")}}>
+                            Edit
+                        </button>
                     </div>
+
                     <div className="TextInput+Button">
-                        <p> PlayStation: <span> Dancin9D0nZ </span> <button className="editButton">  </button> </p>
+                        <p>PlayStation: <span>{this.state.playstationInput}</span></p>
+                        <input className="hide" type="text" id="playstationInput"></input>
+                        <button type="button" className="btn btn-secondary" onClick={() => {this.editAction("playstationInput")}}>
+                            Edit
+                        </button>
                     </div>
+
                     <div className="TextInput+Button">
-                        <p> Xbox: <span> N/A </span> <button className="editButton">  </button> </p>
+                        <p>Xbox: <span>{this.state.xboxInput}</span></p>
+                        <input className="hide" type="text" id="xboxInput"></input>
+                        <button type="button" className="btn btn-secondary" onClick={() => {this.editAction("xboxInput")}}>
+                            Edit
+                        </button>
                     </div>
                 </div>
             </div>
