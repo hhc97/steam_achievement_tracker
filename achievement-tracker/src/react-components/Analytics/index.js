48579778ddbc7fb2e66d697aacc5d80daed53bd8
Haocheng Hu <hc.hu@utoronto.ca>
2021-04-08 00:40:32 -0400

    remove comments

--- achievement-tracker/src/react-components/Analytics/index.js
+++ achievement-tracker/src/react-components/Analytics/index.js
@@ -161,6 +161,7 @@ class Analytics extends React.Component {
         this.updateAchievements()
     }
 
+    // sets the user membership length to a human readable string
     async setMemberAge() {
         let joined
         await fetch(`/users/joindate/${this.state.username}`)
@@ -172,32 +173,6 @@ class Analytics extends React.Component {
         const ageString = prettyMilliseconds(age_ms) + '  '
         const stopPoint = ageString.split(' ', 2).join(' ').length
         this.setState({ joinDate: 'Member for ' + ageString.slice(0, stopPoint) })
-        // manual calculation -- replaced with package
-        // let ageSeconds = (today.getTime() - joinedDate.getTime()) /1000
-        // let age
-        // // if age of account is less than 1 day, report age in hours and minutes
-        // if (ageSeconds < 86400) {
-        //     let ageHours = Math.floor(ageSeconds/3600)
-        //     let ageMinutes = Math.floor(ageSeconds/60 - ageHours*60)
-        //     // display plural 'hours' if age > 2h
-        //     age = `${ageHours} hour${ageHours > 1 ? "s":""} ${ageMinutes} minute${ageMinutes > 1 ? "s":""}`
-        // // if age is greater than 1 day but under 1 month, report age in days
-        // } else if (ageSeconds/86400 < 30) {
-        //     // display plural 'days' if age > 2 days
-        //     age = `${Math.floor(ageSeconds/86400)} day${ageSeconds > 86400*2 ? "s":""}`
-        // // if age is greater than 1 month but under 1 year, report age in months
-        // } else if (ageSeconds/86400 < 365) {
-        //     // display plural 'months' if age > 2 months
-        //     let ageMonths = Math.floor((ageSeconds/86400)/30)
-        //     let ageDays = Math.floor(ageSeconds/86400 - ageMonths*30)
-        //     age = `${Math.floor((ageSeconds/86400)/30)} month${ageSeconds > 86400*60 ? "s":""} day${ageDays > 1 ? "s":""}`
-        // // if age is greater than 1 year, report age in years and months
-        // } else {
-        //     let ageYears = Math.floor((ageSeconds/86400)/365)
-        //     let ageMonths = Math.floor((ageSeconds/86400 - ageYears*365)/30)
-        //     age = `${ageYears} year${ageYears > 1 ? "s":""} ${ageMonths} month${ageMonths > 1 ? "s":""}`
-        // }
-        // this.setState({ joinDate: "Member for " + age})
     }
 
     componentDidMount() {
