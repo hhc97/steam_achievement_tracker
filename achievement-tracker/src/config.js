dfa17666ee60b6fcef1e11ebb8a2f33d181a0e82
Haocheng Hu <hhu@zynga.com>
2021-08-14 11:46:44 -0400

    remove steam key from frontend

--- achievement-tracker/src/config.js
+++ achievement-tracker/src/config.js
@@ -2,31 +2,15 @@
 // Do not store any sensitive data/secret values/API keys here - it is available in the browser to anyone.
 
 
-const API_HOST = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000'
-
-async function getKey() {
-    const url = `${API_HOST}/steamapi/apikey/`;
-    return await fetch(url)
-        .then(res => {
-            return res.text();
-        })
-        .then(text => {
-            return text
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
-
 const prod = {
     env: 'production',
-    api_host: API_HOST, // an empty string to signify a relative path. can also put a deployment URL.
-    steam_key: getKey()
+    api_host: '', // an empty string to signify a relative path. can also put a deployment URL.
+    steam_key: ''
 };
 const dev = {
     env: 'development',
-    api_host: API_HOST, // web server localhost port
-    steam_key: getKey()
+    api_host: 'http://localhost:5000', // web server localhost port
+    steam_key: ''
 };
 
 // export the appropriate environment
