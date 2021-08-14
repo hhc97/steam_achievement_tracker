1d52fc1c8a843542ba7d6eb299561bc0135f9a9e
Haocheng Hu <hhu@zynga.com>
2021-08-14 11:36:28 -0400

    use key directly in server

--- routes/steam.js
+++ routes/steam.js
@@ -5,6 +5,7 @@ const log = console.log
 const express = require('express');
 const router = express.Router(); // Express Router
 const request = require('request')
+const KEY = process.env.STEAM_API_KEY
 
 
 /*** User API routes ****************/
@@ -14,7 +15,7 @@ router.get('/steamapi/userinfo', async (req, res) => {
     const userid = req.query.steamids
 
     const options = {
-        uri: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${userid}`,
+        uri: `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${KEY}&steamids=${userid}`,
         method: 'GET'
     }
 
@@ -27,7 +28,7 @@ router.get('/steamapi/usergames', async (req, res) => {
     const userid = req.session.steamID
 
     const options = {
-        uri: `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${key}&steamid=${userid}
+        uri: `https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key=${KEY}&steamid=${userid}
         &include_appinfo=true&include_played_free_games=true`,
         method: 'GET'
     }
@@ -42,7 +43,7 @@ router.get('/steamapi/games/', async (req, res) => {
     const userid = req.session.steamID
 
     const options = {
-        uri: `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?key=${key}&steamid=${userid}
+        uri: `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v1/?key=${KEY}&steamid=${userid}
         &appid=${appid}`,
         method: 'GET'
     }
@@ -56,17 +57,13 @@ router.get('/steamapi/game/', async (req, res) => {
     const appid = req.query.appid
 
     const options = {
-        uri: `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v1/?key=${key}&appid=${appid}`,
+        uri: `https://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v1/?key=${KEY}&appid=${appid}`,
         method: 'GET'
     }
 
     request(options, function (err, steam_res) { res.send(steam_res.body) })
 })
 
-// route that returns the steam API key
-router.get('/steamapi/apikey/', async (req, res) => {
-    res.send(process.env.STEAM_API_KEY || 'EA00CF15181206B55D12350EB819F943')
-})
 
 // export the router
 module.exports = router
